385efb8dd6baa5b6a8445033c4cba1d3554eaf87
fujian <337045342@qq.com>
2020-11-08 13:45:51 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -11,29 +11,39 @@ export default {
   computed: {
     ...mapState('mall', ['shop_id'])
   },
+  watch: {
+    shop_id(n) {
+      this.init()
+    }
+  },
   mounted() {
-    let id = this.shop_id
-    id && (createSocket(window.SITE_CONFIG.socket + id))
-    window.addEventListener('onmessageWS', (e) => {
-      let data = e.detail.data
-      if (data != 'ping') {
-        if (Notification && Notification.permission === 'granted') {
-          new Notification('未读消息', {
-            body: '您有一条新消息！',
-            icon: require('@/assets/img/xiaoxin.png'),
-            silent: true
-          })
-        } else {
-          this.$notify({
-            title: '未读消息',
-            message: '您有一条新消息！',
-            iconClass: 'notifition-icon',
-            duration: 0,
-            position: 'bottom-right'
-          })
+    this.init()
+  },
+  methods: {
+    init () {
+      let id = this.shop_id
+      id && (createSocket(window.SITE_CONFIG.socket + id))
+      window.addEventListener('onmessageWS', (e) => {
+        let data = e.detail.data
+        if (data != 'ping') {
+          if (Notification && Notification.permission === 'granted') {
+            new Notification('未读消息', {
+              body: '您有一条新消息！',
+              icon: require('@/assets/img/xiaoxin.png'),
+              silent: true
+            })
+          } else {
+            this.$notify({
+              title: '未读消息',
+              message: '您有一条新消息！',
+              iconClass: 'notifition-icon',
+              duration: 0,
+              position: 'bottom-right'
+            })
+          }
         }
-      }
-    })
+      })
+    }
   }
 }
 </script>
